74e92b3ee413855df0ef672f6db8c6ee92513355
zehang <houzehang@zmexing.com>
2023-12-13 14:29:32 +0800

    bugfix

--- socketEvents.js
+++ socketEvents.js
@@ -1,40 +1,52 @@
 const WebSocket = require('ws');
 
-let clientIdCounter = 0; // 用于为客户端分配唯一ID
+let clientIdCounter = 0;
+let bossClients = new Set(); // 用于存储所有boss客户端的集合
 
 module.exports = function (io, server) {
     console.log('DEBUG_LOG: setup wsserver', '');
 
     const wss = new WebSocket.Server({ server });
-    
+
     wss.on('connection', function connection(ws) {
-        const clientId = ++clientIdCounter; // 分配客户端ID
-        ws.clientId = clientId; // 将ID附加到WebSocket实例
+        const clientId = ++clientIdCounter;
+        ws.clientId = clientId;
         console.log(`新的客户端已连接，ID: ${clientId}`);
-    
+
         ws.on('message', function incoming(message) {
             console.log(`收到消息：${message.toString()} 从客户端ID: ${clientId}`);
-            
-            // 检查消息是否为广播消息
-            if (message.toString().startsWith('broadcast:')) {
-                // 发送广播消息
-                let broadcastMsg = message.toString().slice('broadcast:'.length);
-                wss.clients.forEach(function each(client) {
-                    if (client.readyState === WebSocket.OPEN) {
-                        client.send(`广播：${broadcastMsg}`);
+
+            if (message.startsWith('identify:boss')) {
+                // 标记为boss客户端
+                bossClients.add(ws);
+                console.log(`客户端ID: ${clientId} 被标记为boss`);
+                return;
+            }
+
+            // 发送消息给boss
+            if (bossClients.size > 0) {
+                bossClients.forEach(boss => {
+                    if (boss.readyState === WebSocket.OPEN) {
+                        console.log(`来自客户端ID ${clientId} 的消息: ${message}`);
+                        boss.send(message)
                     }
                 });
             } else {
-                // 点对点消息回应
-                ws.send(`收到：${message} (来自您的ID: ${clientId})`);
+                let message = {
+                    type: "tip",
+                    msg: "当前没有boss在线"
+                }
+                ws.send(JSON.stringify(message));
             }
         });
-    
+
         ws.on('close', function close() {
             console.log(`客户端ID: ${clientId} 已断开连接`);
+            // 从boss客户端集合中移除
+            bossClients.delete(ws);
         });
     });
-    
+
     wss.on('error', function error(err) {
         console.error('WebSocket 服务器发生错误:', err);
     });
