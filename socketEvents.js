af5c71d3d2a6e755980b1f91ae37e56b9f8d5b32
zehang <houzehang@zmexing.com>
2023-12-14 10:45:37 +0800

    bugfix

--- socketEvents.js
+++ socketEvents.js
@@ -17,19 +17,19 @@ function applyDiff(text1, changes) {
     let text1List = [...text1];
 
     // Sort 'remove' changes in descending order of position
-    changes.remove.sort((a, b) => b.pos - a.pos);
+    changes.remove.sort((a, b) => b.p - a.p);
 
     // Apply removals
     changes.remove.forEach(change => {
-        text1List.splice(change.pos, change.words.length);
+        text1List.splice(change.p, change.w.length);
     });
 
     // Sort 'add' changes in ascending order of position
-    changes.add.sort((a, b) => a.pos - b.pos);
+    changes.add.sort((a, b) => a.p - b.p);
 
     // Apply additions
     changes.add.forEach(change => {
-        text1List.splice(change.pos, 0, ...change.words);
+        text1List.splice(change.p, 0, ...change.w);
     });
 
     return text1List.join('');
